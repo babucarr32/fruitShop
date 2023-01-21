a343d607d705de517ab0ac4e28bee77d467b8b67
babucarr32 <baboucarry@gmail.com>
2023-01-21 17:20:57 +0000

    Created the dom

---
+++ index.js
@@ -0,0 +1,52 @@
+import { imageNames } from "./indexPlus.js";
+for(let fileName of imageNames){
+    console.log(fileName);
+}
+// Creating DOM for images
+/*
+<div class="image">
+    <img class="fruitImageGrid" src="./images/apple-g517729a42_1920.jpg" alt="">
+    <div class="fruitTitleNcart">
+        <div class="fruitTitleDiv">
+            <h3 id="fruitTitle">Orange</h3>
+        </div>
+        <div class="addToCart">
+            <a class="addToCartAnchor" href="#">
+                <span class="material-symbols-outlined" style="font-size: 30px;">add</span>
+            </a>
+        </div>
+    </div>
+<div>
+*/
+function createProduct(img){
+    const mainImageGrid = document.createElement('div');
+    mainImageGrid.className = 'image';
+    const image = document.createElement('img');
+    image.className = 'fruitImageGrid';
+    image.src = `./images/${img}`;
+    // Create fruit and title div
+    const fruitTitleNcart = document.createElement('div');
+    fruitTitleNcart.className = 'fruitTitleNcart';
+    const fruitTitleDiv = document.createElement('div');
+    fruitTitleDiv.className = 'fruitTitleDiv';
+    const fruitTitle = document.createElement('h3');
+    fruitTitle.className = 'fruitTitle';
+    //Create add to cart icon
+    const addToCart =  document.createElement('div');
+    addToCart.className = 'addToCart';
+    const addToCartAnchor = document.createElement('a');
+    addToCartAnchor.className = 'addToCartAnchor';
+    const materialSymbol = document.createElement('span');
+    materialSymbol.className = 'material-symbols-outlined';
+    materialSymbol.style.fontSize = '30px';
+    materialSymbol.innerText = 'add';
+
+    //nesting the elemnts
+    addToCartAnchor.append(materialSymbol);
+    addToCart.append(addToCartAnchor);
+    fruitTitleDiv.append(fruitTitle);
+    fruitTitleNcart.append(addToCart, fruitTitleDiv);
+    mainImageGrid.append(image, fruitTitleNcart);
+    console.log(mainImageGrid.outerHTML);
+}
+createProduct('meal-gf9c21626a_1920.png')
