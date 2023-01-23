e0a496b01e03f55b2c3d4c0deefad4924421ef8a
babucarr32 <baboucarry@gmail.com>
2023-01-23 23:30:46 +0000

    Added the localStorage.getitem in cart.js

--- cart.js
+++ cart.js
@@ -1,3 +1,14 @@
 import { test } from "./indexPlus.js";
 // import { imageNames } from "./indexPlus.js";
 // console.log(test());
+
+let items = localStorage.getItem('items');
+if(items){
+    items = JSON.parse(items);
+    for(let x of items){
+        console.log(x);
+    }
+}
+else{
+    items = [];
+}
