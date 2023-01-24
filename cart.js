92c84b7bf0cbc2bca2acbbe6da92b0d5897e167f
babucarr32 <baboucarry@gmail.com>
2023-01-24 23:05:20 +0000

    Re-edited the cart list

--- cart.js
+++ cart.js
@@ -18,22 +18,43 @@ else{
 document.querySelector('#total').innerText = `$${totalCost}`;
 
 // Create cart objects
+/*
+<li>
+    <div class="flexContainer">
+        <div class="cartLeft">
+            <p class="noMargin"><b>Orange</b></p>
+            <img src="./images/apple-g517729a42_1920.jpg" alt="">
+        </div>
+        <div class="cartRight">
+            <p class="amountAdded noMargin"><span style="color: black">Amount:</span>  +3</p>
+            <a href="#">
+                <span class="material-symbols-outlined cart delete" >delete_outline</span>
+            </a>
+        </div>
+    </div>
+</li>
+*/
 function cartElements(name, imageName, price){
     const listItem = document.createElement('li');
+    const flexContainer = document.createElement('div');
+    flexContainer.className = 'flexContainer';
     const cartLeft = document.createElement('div');
     cartLeft.className = 'cartLeft';
-    const fruitName = document.createElement('h3');
-    fruitName.innerText = `${name}`;
+    const fruitName = document.createElement('p');
+    fruitName.className = 'noMargin';
+    const boldFruitName = document.createElement('b');
+    boldFruitName.innerText = `${name}`;
     const fruitImage = document.createElement('img');
-    fruitImage.className = 'fruitImageGrid';
     fruitImage.src = `./images/${imageName}`;
     
     const cartRight = document.createElement('div');
     cartRight.className = 'cartRight';
-    const totalAmount = document.createElement('h3');
+    const totalAmount = document.createElement('p');
+    totalAmount.className = 'amountAdded noMargin';
     totalAmount.innerText = 'Amount: ';
     const totalSpan = document.createElement('span');
     totalSpan.innerText = `$${price}`;
+    totalSpan.style.color = '#0066ff'
     const aTagforDelIcon = document.createElement('a');
     aTagforDelIcon.href = '#';
     const Icon = document.createElement('span');
@@ -44,8 +65,10 @@ function cartElements(name, imageName, price){
     aTagforDelIcon.append(Icon);
     totalAmount.append(totalSpan)
     cartRight.append(totalAmount, aTagforDelIcon);
+    fruitName.append(boldFruitName);
     cartLeft.append(fruitName, fruitImage);
-    listItem.append(cartLeft, cartRight);
+    flexContainer.append(cartLeft, cartRight);
+    listItem.append(flexContainer)
     // console.log(listItem);
     return listItem;
 }
