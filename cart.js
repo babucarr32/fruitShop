33a3daf999053f0f5391d656255bfd920ddabc50
babucarr32 <baboucarry@gmail.com>
2023-01-24 20:44:42 +0000

    Added the show price and fruit image in cart

--- cart.js
+++ cart.js
@@ -1,14 +1,48 @@
-import { test } from "./indexPlus.js";
+// import { test } from "./indexPlus.js";
 // import { imageNames } from "./indexPlus.js";
 // console.log(test());
 
 let items = localStorage.getItem('items');
 if(items){
     items = JSON.parse(items);
-    for(let x of items){
-        console.log(x);
+    for(let item of items){
+        console.log(item);
+        document.querySelector('#cartItems').append(cartElements(item.name, item.imageName, item.price));
     }
 }
 else{
     items = [];
+}
+
+// Create cart objects
+function cartElements(name, imageName, price){
+    const listItem = document.createElement('li');
+    const cartLeft = document.createElement('div');
+    cartLeft.className = 'cartLeft';
+    const fruitName = document.createElement('h3');
+    fruitName.innerText = `${name}`;
+    const fruitImage = document.createElement('img');
+    fruitImage.className = 'fruitImageGrid';
+    fruitImage.src = `./images/${imageName}`;
+    
+    const cartRight = document.createElement('div');
+    cartRight.className = 'cartRight';
+    const totalAmount = document.createElement('h3');
+    totalAmount.innerText = 'Amount: ';
+    const totalSpan = document.createElement('span');
+    totalSpan.innerText = `${price}`;
+    const aTagforDelIcon = document.createElement('a');
+    aTagforDelIcon.href = '#';
+    const Icon = document.createElement('span');
+    Icon.className = 'material-symbols-outlined cart delete';
+    Icon.innerText = 'delete_outline';
+    
+    // Append the elements to each other
+    aTagforDelIcon.append(Icon);
+    totalAmount.append(totalSpan)
+    cartRight.append(totalAmount, aTagforDelIcon);
+    cartLeft.append(fruitName, fruitImage);
+    listItem.append(cartLeft, cartRight);
+    // console.log(listItem);
+    return listItem;
 }
